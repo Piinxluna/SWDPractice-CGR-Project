8691096413c7c9782f2223723a16fffe39019cdd
Wuaimi <141891526+Wuaimi@users.noreply.github.com>
2024-03-03 14:08:38 +0700

    getBookedReserves Update

--- routes/reserves.js
+++ routes/reserves.js
@@ -5,14 +5,20 @@ const {
   deleteReserve,
   getReserve,
   updateReserve,
+  getBookedReserves,
 } = require('../controllers/reserves')
 
 const router = express.Router({ mergeParams: true })
 
 const { protect, authorize } = require('../middleware/auth')
 
-router.route('/').get(protect, getReserves).post(protect, createReserve)
-router.route('/:rid').delete(protect, deleteReserve)
+router.route('/')
+.get(protect, getReserves)
+.post(protect, createReserve)
+router.route('/booked-reserves')
+.get(getBookedReserves)
+router.route('/:rid')
+.delete(protect, deleteReserve)
 .get(protect , authorize('admin','customer') ,getReserve)
 .put(protect , authorize('admin','user'), updateReserve)
 module.exports = router
