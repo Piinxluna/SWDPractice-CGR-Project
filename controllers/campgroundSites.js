88ab0d6857023448c4869bfcd96caa428694d231
Supawit <supawit.ma@gmail.com>
2024-03-04 12:31:10 +0700

    add pangination

--- controllers/campgroundSites.js
+++ controllers/campgroundSites.js
@@ -54,13 +54,78 @@ exports.getCampgroundSites = async (req, res, next) => {
       })
     }
 
+    let query
+
+    //Copy req.query
+    const reqQuery = { ...req.query }
+
+    //Fields to exclude
+    const removeFields = ['select', 'sort', 'page', 'limit']
+
+    //Loop over to remove fields and delete from reqQuery
+    removeFields.forEach((param) => delete reqQuery[param])
+
+    let queryStr = JSON.stringify(reqQuery)
+
+    //Create operator $gt $gte
+    queryStr = queryStr.replace(
+      /\b(gt|get|lt|lte|in)\b/g,
+      (match) => `$${match}`
+    )
+
+    queryJson = JSON.parse(queryStr)
+    queryJson.campground = req.params.cgid
+
+    query = Site.find(queryJson)
+
+    //Select field
+    if (req.query.select) {
+      const fields = req.query.select.split(',').join(' ')
+      query = query.select(fields)
+    }
+
+    //Sort field
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ')
+      query = query.sort(sortBy)
+    } else {
+      query = query.sort('zone')
+    }
+
+    //Pagination
+    const page = parseInt(req.query.page, 10) || 1
+    const limit = parseInt(req.query.limit, 10) || 25
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+    const total = await Site.countDocuments()
+
+    query = query.skip(startIndex).limit(limit)
+
     // Find sites
-    const sites = await Site.find({ campground: req.params.cgid })
+    const sites = await query
+
+    //Pagination result
+    const pagination = {}
+
+    //Check if can goto next or prev page
+    if (endIndex < total) {
+      pagination.next = {
+        page: page + 1,
+        limit,
+      }
+    }
+
+    if (startIndex > 0) {
+      pagination.prev = {
+        page: page - 1,
+        limit,
+      }
+    }
 
     // Send response
     res
       .status(200)
-      .json({ success: true, campground, sites, count: sites.length })
+      .json({ success: true, campground, sites, count: sites.length, pagination})
   } catch (err) {
     console.log(err.stack)
     res.status(400).json({ success: false })
