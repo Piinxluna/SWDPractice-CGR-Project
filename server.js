dcfd2ec23b0af87eee3d61545ce0611d27bcb7fa
Supawit <supawit.ma@gmail.com>
2024-03-24 15:25:23 +0700

    test 5

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use("/api/users", users);
 app.use("/api/reserves", reserves);
 app.use("/api/logs", logs);
 
-app.use(express.static(__dirname + "/campgroundImage"));
+app.use("/images", express.static(path.join(__dirname, "campgroundImage")));
 
 // const test = require('./routes/test')
 // app.use('/test', test)
