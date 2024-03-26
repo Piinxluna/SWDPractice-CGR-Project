3df69546c355e6b4a06f7b9d6c348a07f9a4b1a3
Supawit <supawit.ma@gmail.com>
2024-03-26 22:44:30 +0700

    fixed upload image

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // Import middleware
-const { protect, authorize } = require('../middleware/auth')
+const { protect, authorize } = require("../middleware/auth");
 
 // Import Campground controllers
-const { 
+const {
   getCampgrounds,
   getCampground,
-  createCampground , 
-  updateCampground , 
-  deleteCampground, 
+  createCampground,
+  updateCampground,
+  deleteCampground,
   uploadCampgroundImage,
-} = require('../controllers/campground')
+} = require("../controllers/campground");
 
 // Import Campground controllers
 const {
@@ -20,32 +20,35 @@ const {
   getCampgroundSite,
   deleteCampgroundSite,
   getCampgroundSites,
-  updateCampgroundSite
-} = require('../controllers/campgroundSites')
+  updateCampgroundSite,
+} = require("../controllers/campgroundSites");
 
 // Import others router
-const reservesRouter = require('./reserves')
+const reservesRouter = require("./reserves");
 
 // Reserve router
-router.use('/:cgid/sites/:sid/reserves', reservesRouter)
-router.use('/:cgid/reserves', reservesRouter)
+router.use("/:cgid/sites/:sid/reserves", reservesRouter);
+router.use("/:cgid/reserves", reservesRouter);
 
 // Campground router
-router.route('/')
+router
+  .route("/")
   .get(getCampgrounds)
-  .post(protect , authorize('admin') ,createCampground)
-router.route('/:id')
+  .post(protect, authorize("admin"), createCampground);
+router
+  .route("/:id")
   .get(getCampground)
-  .put(protect , authorize('admin') ,updateCampground)
-  .delete(protect , authorize('admin') , deleteCampground)
-router.route('/:cgid/upload-image')
-  .post(protect, authorize('admin'), uploadCampgroundImage)
-router.route('/:cgid/sites')
+  .put(protect, authorize("admin"), updateCampground)
+  .delete(protect, authorize("admin"), deleteCampground);
+router.route("/:cgid/upload-image").post(uploadCampgroundImage);
+router
+  .route("/:cgid/sites")
   .get(getCampgroundSites)
-  .post(protect , authorize('admin') ,createCampgroundSite)
-router.route('/:cgid/sites/:sid')
+  .post(protect, authorize("admin"), createCampgroundSite);
+router
+  .route("/:cgid/sites/:sid")
   .get(getCampgroundSite)
-  .put(protect, authorize('admin') , updateCampgroundSite)
-  .delete(protect, authorize('admin'), deleteCampgroundSite)
+  .put(protect, authorize("admin"), updateCampgroundSite)
+  .delete(protect, authorize("admin"), deleteCampgroundSite);
 
-module.exports = router
+module.exports = router;
